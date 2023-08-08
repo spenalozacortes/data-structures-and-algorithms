6d465b3ef6364c67e45e96fc63114bde75e7e2ac
Stephanie Peñaloza Cortés <spenalozacortes@gmail.com>
2023-08-08 16:06:25 -0600

    Update node

--- src/Node.java
+++ src/Node.java
@@ -1,34 +1,28 @@
 public class Node {
 
-    public String data;
-    private Node next;	
+  public String data;
+  private Node next;
+  private Node previous;
+
+  public Node(String data) {
+    this.data = data;
+    this.next = null;
+  }
   
-    public Node(String data) {
-      this.data = data;
-      this.next = null;
-    }
-  
-    public void setNextNode(Node node) {
-      this.next = node;
-    }
-  
-    public Node getNextNode() {
-      return this.next;
-    }
-  
-    public static void main(String[] args) {
-      Node strawberry = new Node("Berry Tasty");
-      Node banana = new Node("Banana-rama");
-      Node coconut = new Node("Nuts for Coconut");
-  
-      strawberry.setNextNode(banana);
-      banana.setNextNode(coconut);
-  
-      Node currentNode = strawberry;
-      while (currentNode != null) {
-        System.out.println(currentNode.data);
-        currentNode = currentNode.getNextNode();
-      }
-    }
-  
- }
+  public void setNextNode(Node node) {
+    this.next = node;
+  }
+
+  public void setPreviousNode(Node node) {
+    this.previous = node;
+  }
+
+  public Node getNextNode() {
+    return this.next;
+  }
+
+  public Node getPreviousNode() {
+    return this.previous;
+  }
+
+}
